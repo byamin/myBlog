47c147abde5a5f92c058cdb1a4019b10006f4ba2
symphp <172344455@qq.com>
2017-05-03 17:49:11 +0800

    初始化文件

---
+++ article_list.php
@@ -0,0 +1,36 @@
+<?php
+	include('nav.html');
+?>
+<!DOCTYPE html>
+<div class="container article_content">
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>文章ID</th>
+				<th>文章分类</th>
+				<th>文章图片</th>
+				<th>文章标题</th>
+				<th>添加时间</th>
+				<th>操作</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>前端</td>
+				<td></td>
+				<td>jquery</td>
+				<td>2017-05-20 17:00:01</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>后端</td>
+				<td></td>
+				<td>php</td>
+				<td>2017-05-20 17:00:01</td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
