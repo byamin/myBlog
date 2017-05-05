a800c8986aacb81d4043ee9ac08b64bb7dd5a426
symphp <172344455@qq.com>
2017-05-05 18:02:03 +0800

    设置ueditor路径

--- article_add.php
+++ article_add.php
@@ -2,12 +2,8 @@
 	include ('nav.html');
 ?>
 <!DOCTYPE >
-    <!-- 配置文件 -->
-    <script type="text/javascript" src="ueditor/ueditor.config.js"></script>
-    <!-- 编辑器源码文件 -->
-    <script type="text/javascript" src="ueditor/ueditor.all.js"></script>
 <div id="" class="container">
-	<form action="" method="post">
+	<form action="add" method="post">
 		<div class="form-group">
 			<label for="article_name">文章标题</label>
 	    	<input type="text" class="form-control" id="article_name" placeholder="请输入文章标题">
@@ -28,13 +24,19 @@
 	  		 <script id="editor" name="content" type="text/plain">
     		</script>
 	  	</div>
-	  	<!-- 实例化编辑器 -->
+	  	 <!-- 配置文件 -->
+	    <script type="text/javascript" src="ueditor/ueditor.config.js"></script>
+	    <!-- 编辑器源码文件 -->
+	    <script type="text/javascript" src="ueditor/ueditor.all.js"></script>
+	    	<!-- 实例化编辑器 -->
 		<script type="text/javascript">
 				var ue = UE.getEditor('editor',{
 				        	 autoHeight: false,
+							autoClearinitialContent:true,
+							initialContent:'请输入文章内容',
 				        });
 				ue.ready(function(){
-				        	 ue.setContent('请输入文章内容');
+				    
 				        });
 		</script>
 		<input type="submit" class="btn btn-success" value="添加"/>
